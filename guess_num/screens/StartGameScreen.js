4e2aecc52badb20873e723594f22ca650b3a24ae
Ross <rostyslav.kamyshanov@globallogic.com>
2022-03-22 20:48:32 +0200

    added Card component, StartGameScreen

---
+++ guess_num/screens/StartGameScreen.js
@@ -0,0 +1,48 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'; 
+import React from 'react';
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.screen}>
+        <Text style={styles.title}>The Game Screen</Text>
+        <Card style={styles.inputContainer}>
+            <Text>Select Number:</Text>
+            <TextInput />
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}><Button title='Reset' color={Colors.accent} /></View>
+                <View style={styles.button}><Button title='Confirm' color={Colors.primary}/></View>
+            </View>
+        </Card>
+    </View>
+  )
+}
+
+export default StartGameScreen
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1, 
+        padding: 10, 
+        alignItems: 'center',
+    }, 
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300, 
+        maxWidth: '80%',
+        alignItems: 'center',
+        },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    button: {
+        width: 100
+    }
+})
